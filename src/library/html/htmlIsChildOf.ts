97d7599422c96e747dd987651b02a8c417d47e69
oredan <179danil@gmail.com>
2022-08-18 13:35:54 +0300

    htmlIsChildOf.ts was removed, cause unused

--- src/library/html/htmlIsChildOf.ts
+++
@@ -1,4 +0,0 @@
-export default function htmlIsChildOf (children: Element, parent: Element): boolean {
-  const { parentElement } = children
-  return parentElement ? parentElement === parent || htmlIsChildOf(parentElement, parent) : false
-}
