33266dc572520c3ff0bd1defe6ccf2b1bc289dba
oredan <179danil@gmail.com>
2022-08-22 16:29:46 +0300

    refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import OverDirective from '@/directives/OverDirective'
 import icons from '@/library/icons'
 import useApp from '@/stores/app'
 import { createApp } from 'vue'
@@ -5,29 +6,12 @@ import router from './router'
 import pinia from './stores'
 import App from './App.vue'
 import './styles/index.scss'
-import query from '@/library/query'
 
-const app = createApp(App)
-app.use(pinia)
+createApp(App)
+  .directive('over', OverDirective)
+  .use(pinia)
+  .use(icons)
+  .use(router)
+  .mount('body')
 
-const appStore = useApp()
-
-const _oreum_space = {
-  app,
-  states: {
-    app: appStore
-  },
-  router,
-  query
-}
-
-declare global {
-  interface Window {
-    oreum_space: typeof _oreum_space
-  }
-}
-
-window.oreum_space = _oreum_space
-app.use(icons)
-app.use(router)
-app.mount('body')
+useApp()
